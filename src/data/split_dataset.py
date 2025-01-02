319c04ef44a8ea5fafc105b45b7baf5df273d504
DJOUNANG NANA Guy Rostan <guyrostan_nana@yahoo.fr>
2025-01-02 16:16:01 +0100

    Subset dataset by removing undesirable columns

--- src/data/split_dataset.py
+++ src/data/split_dataset.py
@@ -11,9 +11,10 @@ PROCESSED_DATA_FOLDER = os.getenv("PROCESSED_DATA_FOLDER")
 def split_dataset(input_dataset_filepath, output_data_path):
     print(f"Input data filepath: {input_dataset_filepath}")
     print(f"Output data path: {output_data_path}")
+    column_names = ["ave_flot_air_flow", "ave_flot_level", "iron_feed", "starch_flow", "amina_flow", "ore_pulp_flow", "ore_pulp_pH", "ore_pulp_density"]
     df = pd.read_csv(input_dataset_filepath)
     y = df["silica_concentrate"]
-    X = df.drop(columns=["silica_concentrate"])
+    X = df[column_names]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     X_train.to_csv(f"{output_data_path}/X_train.csv")
     X_test.to_csv(f"{output_data_path}/X_test.csv")
