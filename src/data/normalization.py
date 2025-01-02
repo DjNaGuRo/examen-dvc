591076c4f70a0705b9c791fc9260f13548738b20
DJOUNANG NANA Guy Rostan <guyrostan_nana@yahoo.fr>
2025-01-02 16:31:26 +0100

    Fix scaled data saving issues

--- src/data/normalization.py
+++ src/data/normalization.py
@@ -11,11 +11,12 @@ def scale_data(processed_data_path):
     print(f"Processed data folder: {processed_data_path}")
     X_train = pd.read_csv(f"{processed_data_path}/X_train.csv")
     X_test = pd.read_csv(f"{processed_data_path}/X_test.csv")
+    column_names = X_test.columns
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
-    X_train_scaled.to_csv(f"{processed_data_path}/X_train_scaled.csv")
-    X_test_scaled.to_csv(f"{processed_data_path}/X_test_scaled.csv")
+    pd.DataFrame(data=X_train_scaled, columns=column_names).to_csv(f"{processed_data_path}/X_train_scaled.csv")
+    pd.DataFrame(data=X_test_scaled, columns=column_names).to_csv(f"{processed_data_path}/X_test_scaled.csv")
 
 
 if __name__ == "__main__":
